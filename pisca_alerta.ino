0b591369c6d41fc4d9b1f33e2edff7b99a3b8d47
pedrohenrique <pedrofukis2003@gmail.com>
2025-03-20 15:19:34 -0300

    acendendo led

--- pisca_alerta.ino
+++ pisca_alerta.ino
@@ -1,1 +1,13 @@
 //DECLARAÇÃO DE VARIÁVEIS
+int led = 3;
+
+void setup(){
+    pinMode(led, OUTPUT);
+}
+
+void(){
+    digitalWrite(led,HIGH);
+    delay(1000);
+    digitalWrite(led,LOW);
+    delay(1000);
+}
