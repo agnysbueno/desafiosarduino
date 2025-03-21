6ca9c6a03a255cc8dca286c2e8223c3514fd56ff
evelynsilvasantos45@gmail.com <isabellazbastos@edu.univali.br>
2025-03-21 09:10:37 -0300

    programação lixeira inteligente

---
+++ lixeira_inteligente.ino
@@ -0,0 +1,24 @@
+// DECLARAÇÃO DE VARIÁVEIS//
+#include <Servo.h>
+Servo motor;
+
+int botao = 6;
+
+void setup(){
+    motor.attach(9); //porta
+    pinMode(botao, INPUT);
+    motor.write(0); //ângulo
+}
+
+void loop(){
+    if(digitalRead(botao) == HIGH){
+        motor.write(180);
+        delay(10000);
+        motor.write(0);
+        
+    }
+
+    if(digitalRead(botao) == LOW){
+        motor.write(0);
+    }
+}
