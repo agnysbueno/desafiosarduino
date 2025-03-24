80033155db2037fa8699c51c4edb7a2ab45db42f
larimaker <laricoradini12@gmail.com>
2025-03-24 10:48:29 -0300

    código base do sensor ultrassônico COM variável auxiliar

--- ultrasonic.ino
+++ ultrasonic.ino
@@ -7,25 +7,22 @@ Ultrasonic sensor_distancia(5,6); //instancia um objeto ultrasonic chamado senso
                                   //porta trig e porta echo, respectivamente
 Servo motor; //instancia um objeto servo chamado 'motor'
 
-/*************** VARIÁVEIS AUXILIARES ***************/
-int distancia; //variável auxiliar que guardará o valor lido
-
 void setup() {
     //não é necessário declarar pinMode para o trig e echo pois a própria biblioteca já faz isso automáticamente
     motor.atach(9); //declara a porta do arduino em que o motor está conectado
 }
 
 void loop() {
-    distancia = sensor_distancia.read 
-    /*o método .read() aciona o trigger do sensor e faz o cálculo da distância, 
-    este valor em cm será guardado na variável 'distancia'
-    */
-
     /*Se a distancia lida pelo sensor for menor que 10cm, 
     então o motor moverá para o ângulo 90, fazendo a lixeira abrir.
     Caso a distância lida seja igual ou maior que 10cm, 
     o motor moverá para o ângulo 0, fazedo a lixeira fechar*/
-    if(distancia < 10){ 
+    if( (distancia = sensor_distancia.read) < 10){ 
+
+         /*o método .read() aciona o trigger do sensor e faz o cálculo da distância, 
+        este valor em cm será guardado na variável 'distancia'
+        */
+
         motor.write(90); //abre a lixeira
     }else{
         motor.write(90); //fecha a lixeira
